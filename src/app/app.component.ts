4c582372e192b30fb73a26f5bf85e652e31cd134
Lawrence Yirenkyi-Boafo <lawrencekyb@gmail.com>
2023-04-01 15:34:12 +0000

    minor update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,28 @@ export class AppComponent {
     year: new FormControl('', [Validators.required, Validators.max(2023)]),
 
   })
+  yy!:number;
+  mm!:number;
   
-  years: number = NaN;
-  months: number = NaN;
-  days: number = NaN;
+  years: any = "--";
+  months: any = "--";
+  days: any = "--";
   constructor() {
   }
+  
 
   onsubmit() {
+const today = new Date(); // Create a new Date object with the current date and time
+const currentMonth = today.getMonth() + 1; // Get the current month (add 1 because getMonth() returns 0-based month index)
+// const startOfYear = new Date(today.getFullYear(), 0, 0); // Create a new Date object for the start of the year
+// const daysInYear = Math.floor((today.getTime() - startOfYear.getTime()) / 86400000); // Calculate the number of days since the start of the year
+
+// console.log(`Today is the ${daysInYear}th day of the year`); // Output: "Today is the 90th day of the year" (for example)
+
+const currentYear = today.getFullYear(); 
+    console.log(currentYear)
+   this.years= currentYear-this.yy
+   this.months= this.mm-currentMonth
    
   }
 }
